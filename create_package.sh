6eacc3d1ba803416b41d193e1c264ffd37365bee
Darren Kessner <dkessner@ucla.edu>
2012-08-10 10:50:17 -0700

    added platform to archive names

--- create_package.sh
+++ create_package.sh
@@ -2,11 +2,22 @@
 
 datestamp=$(date +%y%m%d)
 
+if [ $(uname) == "Darwin" ]; then
+    platform=osx
+else
+    platform=linux
+fi
+
+filename_archive=simrecomb_${platform}_${datestamp}.zip
+
+echo Building docs.
 pushd docs
 pdflatex simrecomb_docs.tex > /dev/null
 popd
 
+echo Building executables.
 bjam
 
-zip -j simrecomb_$datestamp.zip bin/simrecomb bin/subsample_population bin/recombine_data docs/simrecomb_docs.pdf genetic_map_chr21_b36.txt
+echo Creating $filename_archive.
+zip -j $filename_archive bin/simrecomb bin/subsample_population bin/recombine_data docs/simrecomb_docs.pdf genetic_map_chr21_b36.txt
 
